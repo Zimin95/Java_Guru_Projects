d86a6d567503632609f678d0b56f489ded7ace49
kolja <nikolayzimin95@gmail.com>
2023-10-14 19:12:37 +0300

    add

---
+++ JGP/src/main/java/classworks/addition.java
@@ -0,0 +1,12 @@
+package src.main.java.classworks;
+
+public class addition {
+    public static void main(String[] args) {
+        int addendum1 =12;
+        int addendum2= 17;
+        System.out.println("addition exercise");
+        int sum;
+        sum = addendum1+addendum2;
+        System.out.println(sum);
+    }
+}
---
+++ JGP/src/main/java/myAge.java
@@ -0,0 +1,9 @@
+package src.main.java;
+
+public class myAge {
+    public static void main(String[] args) {
+        float myAge;
+        myAge=28;
+        System.out.println(myAge);
+    }
+}
