c1d4ca2d7f606bd3679d3305b4078552b15832bf
kolja <nikolayzimin95@gmail.com>
2023-10-20 12:26:01 +0300

    updates

---
+++ JGP/src/main/java/homeworks/HW5/starsNestedLoops.java
@@ -0,0 +1,12 @@
+package src.main.java.homeworks.HW5;
+
+public class starsNestedLoops {
+    public static void main(String[] args) {
+        for (int a = 0; a < 8; a++) {
+            for (int b = 0; b < 15; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
